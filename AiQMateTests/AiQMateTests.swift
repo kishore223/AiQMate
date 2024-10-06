661f1b7dbfac4f199d5d4db01d0f57ff7b7ce1be
Kishore Murugan <kishoremuruganmk@gmail.com>
2024-10-05 22:07:37 -0500

    Initial Commit

---
+++ AiQMateTests/AiQMateTests.swift
@@ -0,0 +1,17 @@
+//
+//  AiQMateTests.swift
+//  AiQMateTests
+//
+//  Created by Kishore Murugan on 10/5/24.
+//
+
+import Testing
+@testable import AiQMate
+
+struct AiQMateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
